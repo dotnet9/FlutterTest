7541bfce5b708e9ee1bd6998580bd0754bcba555
Dotnet9(dotnet9.com) <1012434131@qq.com>
2021-06-28 16:03:03 +0800

    add scroll code;

--- src/flutter_trip/lib/pages/home_page.dart
+++ src/flutter_trip/lib/pages/home_page.dart
@@ -10,28 +10,54 @@ class _HomePageState extends State<HomePage> {
   List _imageUrls = [
     'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2020/09/Controls.Dark_-1024x576.png',
     'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2020/02/05_AduSkin_SuperEvil.png',
-    'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2019/12/4_Overview-dark-1024x810.png'
+    'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2020/02/04_AduSkin_HeartFM.png'
   ];
+
+  _onScroll(offset) {
+    print(offset);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 160,
-              child: Swiper(
-                itemCount: _imageUrls.length,
-                autoplay: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _imageUrls[index],
-                    fit: BoxFit.fill,
-                  );
-                },
+      body: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: NotificationListener(
+          onNotification: (scrollNotification) {
+            if (scrollNotification is ScrollUpdateNotification &&
+                scrollNotification.depth == 0) {
+              // 滚动且是列表滚动的时候
+              _onScroll(scrollNotification.metrics.pixels);
+              return true;
+            }
+
+            return false;
+          },
+          child: ListView(
+            children: <Widget>[
+              Container(
+                height: 160,
+                child: Swiper(
+                  itemCount: _imageUrls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  pagination: SwiperPagination(),
+                ),
               ),
-            )
-          ],
+              Container(
+                height: 800,
+                child: ListTile(
+                  title: Text('test'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
