f7d7c0d9b5dde1103f44833edc3f0433b2502afb
liushiquan <liushiquan@islide.cc>
2021-06-28 17:26:51 +0800

    Update home_page.dart

--- src/flutter_trip/lib/pages/home_page.dart
+++ src/flutter_trip/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
+const APPBAR_SCRoll_offseT = 100;
+
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _HomePageState();
@@ -12,53 +14,80 @@ class _HomePageState extends State<HomePage> {
     'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2019/12/4_Overview-dark-1024x810.png',
     'https://git.imweb.io/dotnet9/imgs/raw/master/dotnet9_com/wp-content/uploads/2020/02/04_AduSkin_HeartFM.png'
   ];
+  double appBarAlpha = 0;
 
   _onScroll(offset) {
-    print(offset);
+    double alpha = offset / APPBAR_SCRoll_offseT;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MediaQuery.removePadding(
-        removeTop: true,
-        context: context,
-        child: NotificationListener(
-          onNotification: (scrollNotification) {
-            if (scrollNotification is ScrollUpdateNotification &&
-                scrollNotification.depth == 0) {
-              // 滚动且是列表滚动的时候
-              _onScroll(scrollNotification.metrics.pixels);
-              return true;
-            }
+      body: Stack(
+        children: [
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  // 滚动且是列表滚动的时候
+                  _onScroll(scrollNotification.metrics.pixels);
+                  return false;
+                }
 
-            return false;
-          },
-          child: ListView(
-            children: <Widget>[
-              Container(
-                height: 160,
-                child: Swiper(
-                  itemCount: _imageUrls.length,
-                  autoplay: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Image.network(
-                      _imageUrls[index],
-                      fit: BoxFit.fill,
-                    );
-                  },
-                  pagination: SwiperPagination(),
+                return true;
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('test'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
                 ),
               ),
-              Container(
-                height: 800,
-                child: ListTile(
-                  title: Text('test'),
-                ),
-              )
-            ],
-          ),
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
