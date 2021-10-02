bfd14ac291b0bfa1e5fa98194aaa93b67a0fa49d
Dotnet9(dotnet9.com) <1012434131@qq.com>
2021-10-02 12:32:25 +0800

    add screen util;

--- src/flutter_jdshop/lib/main.dart
+++ src/flutter_jdshop/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_jdshop/routers/router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +16,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      //home: Tabs(),
-      initialRoute: '/',
-      onGenerateRoute: onGenerateRoute
+    return ScreenUtilInit(
+      designSize: const Size(750, 1334),  // 配置设计稿的宽度高度
+      builder: () => const MaterialApp(
+        //home: Tabs(),
+        initialRoute: '/',
+        onGenerateRoute: onGenerateRoute
+      ),
     );
   }
 }
--- src/flutter_jdshop/lib/pages/tabs/home_page.dart
+++ src/flutter_jdshop/lib/pages/tabs/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,11 +38,36 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _titleWidget(value) {
+    return Container(
+      height: ScreenUtil().setHeight(34),
+      margin: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      padding: EdgeInsets.only(left: ScreenUtil().setWidth(20)),
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(
+            color: Colors.red,
+            width: ScreenUtil().setWidth(10),
+          )
+        )
+      ),
+      child: Text(value, style: TextStyle(
+        color: Colors.black54,
+        fontSize: ScreenUtil().setSp(26)
+      ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
         _swiperWidget(),
+        const SizedBox(height: 10),
+        _titleWidget('猜你喜欢'),
+        const SizedBox(height: 10),
+        _titleWidget('热门推荐')
       ],
     );
   }
